cf0f0ec9f69516bf31555cca1af6149bc5bddd5f
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-09-25 15:28:24 +0200

    implement Customer

---
+++ src/main/java/ace/model/classes/Customer.java
@@ -0,0 +1,83 @@
+package ace.model.classes;
+
+import ace.model.interfaces.ICustomer;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Customer implements ICustomer
+{
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+
+    public Customer(UUID id, String firstName, String lastName, LocalDate birthDate, Gender gender)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public LocalDate getBirthDate()
+    {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    @Override
+    public Gender getGender()
+    {
+        return this.gender;
+    }
+
+    @Override
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    @Override
+    public void setBirthDate(LocalDate birtDate)
+    {
+        this.birthDate = birtDate;
+    }
+
+    @Override
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public void setGender(Gender gender)
+    {
+        this.gender = gender;
+    }
+
+    @Override
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public UUID getId()
+    {
+        return this.id;
+    }
+
+    @Override
+    public void setId(UUID id)
+    {
+        this.id = id;
+    }
+}
