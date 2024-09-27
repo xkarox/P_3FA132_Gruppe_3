392d60ab569718d2916d0c865e03a0bf72f74d04
Chris <ChristopherWalther@outlook.com>
2024-09-27 09:13:14 +0200

    #20_jddk_implement_basics

--- src/main/java/ace/database/DatabaseConnection.java
+++ src/main/java/ace/database/DatabaseConnection.java
@@ -1,6 +1,6 @@
-package ace.model;
+package ace.database;
 
-import ace.model.interfaces.IDatabaseConnection;
+import ace.database.intefaces.IDatabaseConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
