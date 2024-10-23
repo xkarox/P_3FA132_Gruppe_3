be8618661445ce38501a0022c3c2f52375892303
Erik Mielke <erikmielke.2@gmail.com>
2024-10-23 08:35:53 +0200

    #25_borris123_implement_unit_test_for_create_customer

--- src/test/java/ace/database/services/CustomerServiceTest.java
+++ src/test/java/ace/database/services/CustomerServiceTest.java
@@ -1,16 +1,37 @@
 package ace.database.services;
 
+import ace.model.classes.Customer;
+import ace.model.interfaces.ICustomer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CustomerServiceTest
 {
+    private Customer _testCustomer;
+    private CustomerService _customerService;
     @BeforeEach
     void SetUp() {
+        this._testCustomer = new Customer(UUID.randomUUID(), "John", "Doe", LocalDate.now(), ICustomer.Gender.M);
 
     }
 
     @Test
-    void testAdd()
+    void testAdd() {
+        this._customerService.add(this._testCustomer);
+
+        Customer customerFromDb = this._customerService.getById(this._testCustomer.getId());
+        assertNotNull(customerFromDb);
+        assertEquals(_testCustomer.getId(), customerFromDb.getId());
+        assertEquals(_testCustomer.getFirstName(), customerFromDb.getFirstName());
+        assertEquals(_testCustomer.getLastName(), customerFromDb.getLastName());
+        assertEquals(_testCustomer.getBirthDate(), customerFromDb.getBirthDate());
+        assertEquals(_testCustomer.getGender(), customerFromDb.getGender());
+    }
 
 }
