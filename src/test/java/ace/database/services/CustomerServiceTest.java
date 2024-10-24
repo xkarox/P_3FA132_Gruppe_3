6b9b3eb5a94c5461951da249deff52a80feb05fe
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-10-24 08:41:02 +0200

    44_xkarox_impl_customer_update_test

--- src/test/java/ace/database/services/CustomerServiceTest.java
+++ src/test/java/ace/database/services/CustomerServiceTest.java
@@ -14,6 +14,8 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerServiceTest
 {
     private Customer _testCustomer;
@@ -25,42 +27,32 @@ public class CustomerServiceTest
         this._testCustomer = new Customer(UUID.randomUUID(), "John", "Doe", LocalDate.now(), ICustomer.Gender.M);
         this._databaseConnection = new DatabaseConnection();
         this._databaseConnection.openConnection(DbHelperService.loadProperties());
-        this._customerService = new CustomerService(_databaseConnection);
+        this._customerService = new CustomerService(this._databaseConnection);
     }
-//unfinished
+
     @Test
     void updateTest() {
-//        StringBuilder sb = new StringBuilder("INSERT INTO ").append(_testCustomer.getSerializedTableName())
-//                .append("(id, firstName, lastName, birthDate, gender) VALUES (")
-//                .append(this._testCustomer.getId()).append(",")
-//                .append(this._testCustomer.getFirstName()).append(",")
-//                .append(this._testCustomer.getLastName()).append(",")
-//                .append(this._testCustomer.getBirthDate()).append(",")
-//                .append(this._testCustomer.getGender()).append(",");
-//        this._databaseConnection.executeSqlUpdateCommand(sb.toString());
-//        this._customerService.add(this._testCustomer);
-//
-////        modify customer
-//        this._testCustomer.setFirstName("Peter");
-//        this._testCustomer.setLastName("Griffin");
-//        this._testCustomer.setBirthDate(LocalDate.of(2000, 11, 2));
-//        this._testCustomer.setGender(ICustomer.Gender.W);
-////        update customer
-//        `sb = new StringBuilder("INSERT INTO ").append(_testCustomer.getSerializedTableName())
-//                .append("(id, firstName, lastName, birthDate, gender) VALUES (")
-//                .append(this._testCustomer.getId()).append(",")
-//                .append(this._testCustomer.getFirstName()).append(",")
-//                .append(this._testCustomer.getLastName()).append(",")
-//                .append(this._testCustomer.getBirthDate()).append(",")
-//                .append(this._testCustomer.getGender()).append(",");
-//        this._databaseConnection.executeSqlUpdateCommand(sb.toString());
-////        check if customer is updated correctly
-//        sb = new StringBuilder("SELECT * FROM ").append(this._testCustomer.getSerializedTableName())
-//                .append(" WHERE id=").append(this._testCustomer.getId());
-//        ResultSet customer = this._databaseConnection.executeSqlQueryCommand(sb.toString());
-//        System.out.println(customer);
-////        get Customer
+//        add origin customer
+        this._customerService.add(this._testCustomer);
 
+//        modify customer
+        this._testCustomer.setFirstName("Peter");
+        this._testCustomer.setLastName("Griffin");
+        this._testCustomer.setBirthDate(LocalDate.of(2000, 11, 2));
+        this._testCustomer.setGender(ICustomer.Gender.W);
+//        update customer
+        this._customerService.update(this._testCustomer);
+//        get customer
+        Customer returnedTestCustomer = this._customerService.getById(this._testCustomer.getId());
+//        check if customer is updated correctly
+        assertEquals(this._testCustomer.getFirstName() , returnedTestCustomer.getFirstName()
+                , "First Name should be equal");
+        assertEquals(this._testCustomer.getLastName(), returnedTestCustomer.getLastName()
+                , "Last Name should be equal");
+        assertEquals(this._testCustomer.getBirthDate(), returnedTestCustomer.getBirthDate()
+                , "Birth dates should be equal");
+        assertEquals(this._testCustomer.getGender(), returnedTestCustomer.getGender()
+                , "Gender should be equal");
 
     }
 
