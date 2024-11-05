e3267fbc7769b0711ac464cfe11fc99be607d386
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-11-05 14:38:15 +0100

    88_xkarox_impl_server_start_stop

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,13 +1,22 @@
 package server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 public class Server
 {
+    private static ApplicationContext _appContext;
+
 //    url probably refers to the database url -> ask teacher
-    static void startServer(String url){
-        SpringApplication.run(Main.class);
+    public static void startServer(String url){
+        _appContext = SpringApplication.run(Main.class);
     }
 
-    static void stopServer(){}
+    public static void stopServer()
+    {
+        SpringApplication.exit(_appContext, () -> 0);
+    }
+
 }
