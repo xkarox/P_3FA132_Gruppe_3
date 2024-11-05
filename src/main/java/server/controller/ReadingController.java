50ddea2461f3cec223783430a9f41ae42f6d1fd9
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-11-05 14:38:44 +0100

    88_xkarox_add_base_reading_controller

---
+++ src/main/java/server/controller/ReadingController.java
@@ -0,0 +1,11 @@
+package server.controller;
+
+import ace.model.classes.Customer;
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reading")
+public class ReadingController
+{
+}
