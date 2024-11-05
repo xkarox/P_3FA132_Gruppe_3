94619bddeab1dd8c22fb0b4ef7ebf0c233484873
Erik Mielke <erikmielke.2@gmail.com>
2024-11-05 07:54:52 +0100

    #1_borris123_test

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,12 +1,20 @@
 package server;
+import ace.database.DatabaseConnection;
+import ace.database.services.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws IOException
+    {
+		DatabaseConnection connection = new DatabaseConnection();
+		connection.openConnection();
+		connection.createAllTables();
+		// CustomerService service = new CustomerService(connection);
 		Server.startServer("{{ DatabaseUrl }}");
 	}
 }
