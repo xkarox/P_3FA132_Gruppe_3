3bac1d5ec8ec3a0c49cdd1b1133ce56b86f8043e
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-11-06 18:25:25 +0100

    88_xkarox_refactor_addReading

--- src/main/java/server/controller/ReadingController.java
+++ src/main/java/server/controller/ReadingController.java
@@ -1,12 +1,16 @@
 package server.controller;
 
+import ace.Utils;
 import ace.database.ServiceProvider;
 import ace.database.services.ReadingService;
 import ace.model.classes.Reading;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 
@@ -14,21 +18,30 @@ import java.util.UUID;
 @RequestMapping(value = "/readings")
 public class ReadingController
 {
+    static ObjectMapper _objMapper = Utils.getObjectMapper();
+
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(method = RequestMethod.POST)
-    public Reading postReading(@RequestBody Reading reading){
-        ReadingService rs = ServiceProvider.GetReadingService();
-        if (reading.getId() == null)
-        {
-            reading.setId(UUID.randomUUID());
-        }
+    public Reading addReading(@RequestBody String readingJson)
+    {
         try
         {
-            return rs.add(reading);
+            Reading reading = _objMapper.readValue(readingJson, Reading.class);
+            ReadingService rs = ServiceProvider.GetReadingService();
+            if (reading.getId() == null)
+            {
+                reading.setId(UUID.randomUUID());
+            }
+            reading = rs.add(reading);
+            if (reading == null)
+            {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid reading data provided");
+            }
+            return reading;
         }
         catch (Exception e)
         {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error adding reading", e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid reading data provided");
         }
     }
 }
