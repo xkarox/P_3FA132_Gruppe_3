90d43bae4121097774d01656dcacf15d2cc4b911
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-11-06 18:26:54 +0100

    88_xkarox_add_utility_getObjectMapper

--- src/main/java/ace/Utils.java
+++ src/main/java/ace/Utils.java
@@ -1,5 +1,11 @@
 package ace;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.text.SimpleDateFormat;
+
 public class Utils
 {
     public static <T> void checkValueEquals(T expectedValue, T result, ErrorMessages errorMessage)
@@ -16,5 +22,13 @@ public class Utils
             String resultString = strBuilder.toString();
             throw new IllegalArgumentException(resultString);
         }
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        return objectMapper;
     }
 }
