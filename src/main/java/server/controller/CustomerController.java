063b821eb57455a9280e431bb6842f127e374cad
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-11-06 18:24:11 +0100

    88_xkarox_refactor_addCustomer

--- src/main/java/server/controller/CustomerController.java
+++ src/main/java/server/controller/CustomerController.java
@@ -1,21 +1,44 @@
 package server.controller;
 
+import ace.Utils;
 import ace.database.ServiceProvider;
 import ace.database.services.CustomerService;
 import ace.model.classes.Customer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/customers")
 public class CustomerController {
+    static ObjectMapper _objMapper = Utils.getObjectMapper();
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(method = RequestMethod.POST)
     public Customer postCustomer(@RequestBody Customer customer)
+    public Customer addCustomer(@RequestBody String customerJson)
+    {
+        try
+        {
+            Customer customer = _objMapper.readValue(customerJson, Customer.class);
+            CustomerService cs = ServiceProvider.GetCustomerService();
+            if (customer.getId() == null)
+            {
+                customer.setId(UUID.randomUUID());
+            }
+            return cs.add(customer);
+        }
+        catch (JsonProcessingException | SQLException | RuntimeException e)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer data provided");
+        }
+    }
     {
         CustomerService cs = ServiceProvider.GetCustomerService();
         if (customer.getId() == null)
