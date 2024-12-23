23437804806380dbfa0925d0cc1a40d80f8d7df6
Adrian Grzybek <adrian.grzybek00@gmail.com>
2024-12-23 11:31:01 +0100

    141_xkarox_impl_CORSFilter

---
+++ src/main/java/dev/server/filter/CORSFilter.java
@@ -0,0 +1,22 @@
+package dev.server.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter
+{
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException
+    {
+        MultivaluedMap<String, Object> headers = containerResponseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    }
+}
